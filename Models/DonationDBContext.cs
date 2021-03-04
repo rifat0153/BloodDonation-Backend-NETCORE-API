527607cc9ce19f664189ea06066787020779e3af
Mahbubur Rahman <rifat0153@gmail.com>
2021-03-04 16:37:46 +0600

    BackEnd of Donation Complete

---
+++ Models/DonationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace React_API_2._0.Models
+{
+    public class DonationDBContext: DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<DCandidate> DCandidates { get; set; }
+    }
+}
